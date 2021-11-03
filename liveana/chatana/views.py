67201640d0c0d780d31752eaf056f230e5ecbc5d
MOHO桑 <iam880401@gmail.com>
2021-11-03 12:05:08 +0800

    add form api

--- liveana/chatana/views.py
+++ liveana/chatana/views.py
@@ -3,15 +3,17 @@ from django.shortcuts import render
 from .utils import get_title, chat_record, wExcel
 
 # Create your views here.
-def Home(request):
+
+
+def home(request):
     if request.method == 'POST':
         url = request.POST.get('video_url')
         try:
             title = get_title(url)
-        except:
+        except ValueError:
             print("Url Error")
             return render(request, 'index.html')
-        if title != None:
+        if title:
             request.session['url'] = url
             request.session['title'] = title
             print(request.session['url'], request.session['title'])
--- liveana/liveana/urls.py
+++ liveana/liveana/urls.py
@@ -19,5 +19,5 @@ from chatana import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Home),
+    path('', views.home),
 ]

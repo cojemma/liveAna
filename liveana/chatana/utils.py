a474a8c7686826035b526088ec638e1c635315b5
陳彥儒 <79571751+cojemma@users.noreply.github.com>
2022-01-01 13:32:48 +0800

    modify Html mata tag

--- liveana/chatana/utils.py
+++ liveana/chatana/utils.py
@@ -8,10 +8,7 @@ def chat_record(url, count=None):
         message = chat.format(message)
         message = message.split(' ')
         min = message[0][:-3]
-        if min not in chats_time:
-            chats_time[min] = 1
-        else:
-            chats_time[min] += 1
+        chats_time[min] = chats_time.get(min, 0) + 1
 
     return chats_time
 
@@ -29,7 +26,7 @@ from openpyxl.chart import LineChart, Reference
 def wExcel(data, title='chat-count'):
     wb = openpyxl.Workbook()
     ws = wb.active
-    ws.title = f'{title[:10]}...'
+    ws.title = f'{title[:10]}...' if len(title) > 10 else title
 
     time = [t for t in data]
     count = [data[time] for time in data]
@@ -44,8 +41,8 @@ def wExcel(data, title='chat-count'):
     chart.title = "留言量表"
     chart.x_axis.title = '分鐘'
     chart.y_axis.title = '留言數'
-    data = Reference(ws, min_col=2, min_row=1, max_row=len(ws['B']))
-    chart.add_data(data, titles_from_data=True)
+    chart_data = Reference(ws, min_col=2, min_row=1, max_row=len(ws['B']))
+    chart.add_data(chart_data, titles_from_data=True)
     time = Reference(ws, min_col=1, min_row=2, max_row=len(ws['A']))
     chart.set_categories(time)
 
